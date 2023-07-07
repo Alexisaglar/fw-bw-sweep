4d97fd209b9e4d333c868f4d5ee28eec72910e63
alexys <alexisaglar@gmail.com>
2023-07-07 18:06:33 +0100

    started the algorithm with problems sizing arrays properly

--- bf-sweep.py
+++ bf-sweep.py
@@ -2,9 +2,10 @@ import numpy as np
 import csv
 
 Sbase=100; #MVA
-Vbase=11; #kV
+Vbase=11000; #kV
 Zbase=(Vbase**2)/Sbase
 phases = 3
+PF = 0.95
 
 with open('data/LineCodes.csv', newline='') as linecodefile, open('data/Lines.csv', newline='') as linesfile, open('data/Loads.csv') as loadsfile, open('data/Load Profiles/Load_Profile_1.csv') as profilefile, open('data/Buscoords.csv') as busfile:
     lines_code = np.array(list(csv.reader(linecodefile)), dtype=object)
@@ -51,15 +52,38 @@ for i in range(len(loads_data)):
             if bus_phase == 'B': bus_phase = 1
             if bus_phase == 'C': bus_phase = 2
             Pi[(bus)-1,bus_phase,:] = load_profile[1:]
+#Convert power to Watts instead of kW >
+Pi = Pi*1000
+
+#Create an same size array as apparent power for reactive power 
 Qi = np.zeros((len(bus_coords)-1, phases, len(load_profile)-1))
+#Apply power factor to determine reactive power
+Qi = (Pi/0.95) * np.sin(np.arccos(PF))
 
-print(Pi)
 
+calcNodeVoltages = np.zeros((len(loads_data)-1, phases, 50))
+calcLoadCurrents = np.zeros((len(loads_data)-1, phases, 50))
+calcLineCurrents = np.zeros((len(lines_data)-1, phases, 50))
+#----- Backward forward sweep -----#
+for i in range(len(load_profile)):
+    #Creating starting values for algorithm
+    error = 1
+    epsilon = 0.000001
+    iter = 1 
+    nodeVoltages = np.zeros((len(loads_data)-1, phases, 50))
+    loadCurrents = np.zeros((len(loads_data)-1, phases, 50))
+    lineCurrents = np.zeros((len(lines_data)-1, phases, 50))
     
+    if i == 0:
+        nodeVoltages[:,:,iter] = np.ones((len(loads_data)-1,3))*((1*np.exp(1j*(-2*np.pi/3))*np.exp(1j*(2*np.pi/3)))*Vbase)
+    else:
+        nodeVoltages[:,:,iter] = calcNodeVoltages[:,:,i-1]
+    
+    while error > epsilon:
+        iter = iter+1
+        if iter > 100: 
+            print('Error') 
+            break
+        loadCurrents[:,:,iter] = np.conj((Pi[:,:,i]+1j*Qi[:,:,i])/nodeVoltages[:,:,i-1]) #Calculate load current from S/V
 
-#print(Pi[905,1,0])
-#print(Pi)
 
-V_abc = [] 
-I_abc = []
-Z = []
